01eb2fccbc7a6ba2d8fec86ca3a7f36dc488e919
ZhouCX117 <13155272911@163.com>
2023-06-20 11:37:14 +0800

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,60 @@
+import os
+import sys
+
+import torch
+
+
+class Logger(object):
+  def __init__(self, fpath=None):
+    self.console = sys.stdout
+    self.file = None
+    if fpath is not None:
+      self.file = open(fpath, 'w')
+
+  def __del__(self):
+    self.close()
+
+  def __enter__(self):
+    pass
+
+  def __exit__(self, *args):
+    self.close()
+
+  def write(self, msg):
+    self.console.write(msg)
+    if self.file is not None:
+        self.file.write(msg)
+
+  def flush(self):
+    self.console.flush()
+    if self.file is not None:
+        self.file.flush()
+        os.fsync(self.file.fileno())
+
+  def close(self):
+    self.console.close()
+    if self.file is not None:
+        self.file.close()
+
+class Averagvalue(object):
+    """Computes and stores the average and current value"""
+
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+def save_checkpoint(state, filename='checkpoint.pth'):
+    torch.save(state, filename)
+
+
